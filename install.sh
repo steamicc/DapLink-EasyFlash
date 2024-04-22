8c6345377e7892c6eba47f0c38e70540e0a06689
Jonathan BAUDIN <jjbaudin@gmail.com>
2024-04-22 17:29:01 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -5,27 +5,25 @@ NC='\033[0m' # No Color
 
 
 echo -e "${GREEN}=> Installing system dependency${NC}"
-sudo apt install openocd python3 python3-pip python3-tk
+sudo apt install openocd python3 python3-pip python3-venv
 
-echo -e "\n\n\n${GREEN}=> Virtual env.${NC}"
-echo -e "\n\n    ${ORANGE}Installing virtualenv (pip)${NC}"
-pip install virtualenv | sed 's/^/    /'
+echo -e "\n${GREEN}=> Virtual env.${NC}"
 
-echo -e "\n\n    ${ORANGE}Create virtual env${NC}"
-virtualenv venv | sed 's/^/    /'
+echo -e "\n    ${ORANGE}Create virtual env${NC}"
+python3 -m venv venv | sed 's/^/    /'
 
-echo -e "\n\n\n${GREEN}=> Enter in venv${NC}"
+echo -e "\n${GREEN}=> Enter in venv${NC}"
 source ./venv/bin/activate
 
-echo -e "\n\n\n${GREEN}=> Python depencies${NC}"
-pip install pysimplegui psutil
+echo -e "\n${GREEN}=> Python depencies${NC}"
+pip install tk psutil
 
-echo -e "\n\n    ${ORANGE}pip freeze${NC}"
+echo -e "\n    ${ORANGE}pip freeze${NC}"
 pip freeze | sed 's/^/    /'
 
-echo -e "\n\n\n${GREEN}=> Exit venv${NC}"
+echo -e "\n${GREEN}=> Exit venv${NC}"
 deactivate
 
-echo -e "\n\n\n${GREEN}========= Done ! =========${NC}"
+echo -e "\n${GREEN}========= Done ! =========${NC}"
 read -s -p "Press any key to close ..."
 echo -e "\n\n"
