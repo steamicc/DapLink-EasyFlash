ec98237ba5add7ac7a336c8dfb6d32a062cf39d7
Jonathan BAUDIN <jjbaudin@gmail.com>
2024-10-04 10:49:49 +0200

    refactor(log_widget): Extract "log" to own struct
    This is needed to be used by other class (process can retrun the stdout/stderr)

---
+++ src/log_entries.rs
@@ -0,0 +1,34 @@
+use std::{
+    cell::{Ref, RefCell},
+    collections::VecDeque,
+};
+
+#[derive(Debug, Clone)]
+pub enum LogType {
+    Info(String),
+    Warning(String),
+    Error(String),
+}
+
+#[derive(Debug, Default, Clone)]
+pub struct LogEntries {
+    entries: RefCell<VecDeque<LogType>>,
+}
+
+impl LogEntries {
+    pub fn push(&self, log: LogType) {
+        self.entries.borrow_mut().push_back(log);
+    }
+
+    pub fn pop(&self) -> Option<LogType> {
+        self.entries.borrow_mut().pop_front()
+    }
+
+    pub fn as_deque(&self) -> Ref<VecDeque<LogType>> {
+        self.entries.borrow()
+    }
+
+    pub fn clear(&self) {
+        self.entries.borrow_mut().clear();
+    }
+}
