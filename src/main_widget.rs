6101d36db85ee102a494115412b29a4a400cebfb
Jonathan BAUDIN <jjbaudin@gmail.com>
2024-10-04 18:42:14 +0200

    feat(main_widget): Implement complete process

--- src/main_widget.rs
+++ src/main_widget.rs
@@ -1,22 +1,19 @@
-use std::{path::PathBuf, str::FromStr};
+use std::{fmt::format, path::PathBuf, str::FromStr, time::Duration};
 
 use iced::{
     alignment::Horizontal,
-    widget::{
-        button, center, column, container, opaque, row, stack, text, text_editor, text_input,
-    },
-    Element, Font, Length, Task, Theme,
+    widget::{button, center, column, container, opaque, row, stack, text, text_input},
+    Element, Length, Task, Theme,
 };
 use iced_aw::{grid, grid_row, number_input};
 
 use crate::{
-    log_widget::{LogType, LogWidget},
-    messages::Message,
-    utils,
+    disk_tool, log_entries::LogType, log_widget::LogWidget, messages::Message, open_ocd_task, utils,
 };
 
-const TIMEOUT_MIN: u32 = 100;
-const TIMEOUT_MAX: u32 = 10000;
+const MAINTENANCE_DISK_NAME: &str = "MAINTENANCE";
+const TIMEOUT_MIN: u64 = 1;
+const TIMEOUT_MAX: u64 = 30;
 
 pub struct EasyDapLink {
     theme: Theme,
@@ -24,7 +21,7 @@ pub struct EasyDapLink {
     bootloader_path: PathBuf,
     firmware_path: PathBuf,
     user_file_path: PathBuf,
-    target_waiting_time: u32,
+    target_waiting_time: u64,
     target_name: String,
     log_widget: LogWidget,
 }
@@ -104,13 +101,226 @@ impl EasyDapLink {
             Message::InputUserFilePath(s) => self.user_file_path = PathBuf::from_str(&s).unwrap(),
 
             Message::TimeoutChanged(v) => {
-                self.target_waiting_time = v.clamp(TIMEOUT_MIN, TIMEOUT_MAX)
+                self.target_waiting_time = v.clamp(TIMEOUT_MIN, TIMEOUT_MAX);
             }
 
             Message::TargetNameChanged(s) => self.target_name = s,
 
             Message::StartProcess => {
-                self.validate_fields();
+                if !self.validate_fields() {
+                    return Task::none();
+                }
+
+                match open_ocd_task::is_installed() {
+                    Ok(is_install) => {
+                        if !is_install {
+                            self.log_widget
+                                .push(LogType::Error("OpenOCD is not found".into()));
+                            return Task::none();
+                        }
+                    }
+                    Err(e) => {
+                        self.log_widget.push(LogType::Error(format!(
+                            "Failed to test openocd installation: {e}"
+                        )));
+                        return Task::none();
+                    }
+                };
+
+                self.log_widget.push(LogType::InfoNoPrefix("\n\n".into()));
+                self.log_widget.push(LogType::Info("Unlock target".into()));
+                self.is_readonly = true;
+                return Task::perform(open_ocd_task::unlock_target(), Message::DoneUnlockProcess);
+            }
+
+            Message::DoneProcess => {
+                self.is_readonly = false;
+            }
+
+            Message::DoneUnlockProcess(result) => {
+                if result.is_err() {
+                    self.log_widget.push(LogType::Error(format!(
+                        "Failed to run unlock process. Error: {}",
+                        result.err().unwrap()
+                    )));
+                } else {
+                    let result = result.unwrap();
+
+                    self.log_widget.from_log_entries(&result.log);
+
+                    match result.code {
+                        Some(code) => {
+                            if code == 0 {
+                                self.log_widget.push(LogType::InfoNoPrefix("\n\n".into()));
+                                self.log_widget.push(LogType::Info("Erase target".into()));
+                                return Task::perform(
+                                    open_ocd_task::erase_target(),
+                                    Message::DoneEraseProcess,
+                                );
+                            } else {
+                                self.log_widget
+                                    .push(LogType::Warning(format!("Exit code: {}", code)));
+                            }
+                        }
+                        None => self
+                            .log_widget
+                            .push(LogType::Warning("Process terminated by signal.".into())),
+                    }
+                }
+                return Task::done(Message::DoneProcess);
+            }
+            Message::DoneEraseProcess(result) => {
+                if result.is_err() {
+                    self.log_widget.push(LogType::Error(format!(
+                        "Failed to run erase process. Error: {}",
+                        result.err().unwrap()
+                    )));
+                } else {
+                    let result = result.unwrap();
+
+                    self.log_widget.from_log_entries(&result.log);
+
+                    match result.code {
+                        Some(code) => {
+                            if code == 0 {
+                                self.log_widget.push(LogType::InfoNoPrefix("\n\n".into()));
+                                self.log_widget
+                                    .push(LogType::Info("Flash bootloader".into()));
+
+                                return Task::perform(
+                                    open_ocd_task::flash_target(self.bootloader_path.clone()),
+                                    Message::DoneFlashProcess,
+                                );
+                            } else {
+                                self.log_widget
+                                    .push(LogType::Warning(format!("Exit code: {}", code)));
+                            }
+                        }
+                        None => self
+                            .log_widget
+                            .push(LogType::Warning("Process terminated by signal.".into())),
+                    }
+                }
+                return Task::done(Message::DoneProcess);
+            }
+            Message::DoneFlashProcess(result) => {
+                if result.is_err() {
+                    self.log_widget.push(LogType::Error(format!(
+                        "Failed to run erase process. Error: {}",
+                        result.err().unwrap()
+                    )));
+                } else {
+                    let result = result.unwrap();
+
+                    self.log_widget.from_log_entries(&result.log);
+
+                    match result.code {
+                        Some(code) => {
+                            if code == 0 {
+                                self.log_widget.push(LogType::InfoNoPrefix("\n\n".into()));
+                                self.log_widget.push(LogType::Info(format!(
+                                    "Wait for '{MAINTENANCE_DISK_NAME}' drive"
+                                )));
+                                return Task::perform(
+                                    disk_tool::wait_for_drive(
+                                        MAINTENANCE_DISK_NAME.into(),
+                                        Duration::from_secs(self.target_waiting_time),
+                                    ),
+                                    Message::DoneWaitMaintenanceDisk,
+                                );
+                            } else {
+                                self.log_widget
+                                    .push(LogType::Warning(format!("Exit code: {}", code)));
+                            }
+                        }
+                        None => self
+                            .log_widget
+                            .push(LogType::Warning("Process terminated by signal.".into())),
+                    }
+                }
+                return Task::done(Message::DoneProcess);
+            }
+
+            Message::DoneWaitMaintenanceDisk(is_found) => {
+                if !is_found {
+                    self.log_widget.push(LogType::Error(format!(
+                        "TIMEOUT : The device '{MAINTENANCE_DISK_NAME}' was not found."
+                    )));
+                    return Task::done(Message::DoneProcess);
+                }
+
+                self.log_widget.push(LogType::InfoNoPrefix("\n\n".into()));
+                self.log_widget.push(LogType::Info(format!(
+                    "Copy firmware to {MAINTENANCE_DISK_NAME}"
+                )));
+                return Task::perform(
+                    disk_tool::copy_file_to_disk(
+                        MAINTENANCE_DISK_NAME.into(),
+                        self.firmware_path.clone(),
+                    ),
+                    Message::DoneCopyFirmware,
+                );
+            }
+            Message::DoneCopyFirmware(result) => {
+                match result {
+                    Ok(_) => {
+                        if self.user_file_path.exists() && self.user_file_path.is_file() {
+                            self.log_widget.push(LogType::InfoNoPrefix("\n\n".into()));
+                            self.log_widget.push(LogType::Info(format!(
+                                "Wait for '{}' drive",
+                                self.target_name
+                            )));
+                            return Task::perform(
+                                disk_tool::wait_for_drive(
+                                    self.target_name.clone(),
+                                    Duration::from_secs(self.target_waiting_time),
+                                ),
+                                Message::DoneWaitingDeviceDisk,
+                            );
+                        } else {
+                            self.log_widget
+                                .push(LogType::Warning("No user file. Skip.".into()));
+                        }
+                    }
+                    Err(e) => self
+                        .log_widget
+                        .push(LogType::Error(format!("Copy failed ({e})"))),
+                }
+                return Task::done(Message::DoneProcess);
+            }
+
+            Message::DoneWaitingDeviceDisk(is_found) => {
+                if !is_found {
+                    self.log_widget.push(LogType::Error(format!(
+                        "TIMEOUT : The device '{}' was not found.",
+                        self.target_name
+                    )));
+                    return Task::done(Message::DoneProcess);
+                }
+
+                self.log_widget.push(LogType::InfoNoPrefix("\n\n".into()));
+                self.log_widget.push(LogType::Info(format!(
+                    "Copy firmware to {}",
+                    self.target_name
+                )));
+                return Task::perform(
+                    disk_tool::copy_file_to_disk(
+                        self.target_name.clone(),
+                        self.user_file_path.clone(),
+                    ),
+                    Message::DoneCopyUserfile,
+                );
+            }
+
+            Message::DoneCopyUserfile(result) => {
+                match result {
+                    Ok(_) => (),
+                    Err(e) => self
+                        .log_widget
+                        .push(LogType::Error(format!("Copy failed ({e})"))),
+                }
+                self.log_widget.push(LogType::InfoNoPrefix("\n\n".into()));
+                return Task::done(Message::DoneProcess);
             }
         }
 
@@ -170,13 +380,13 @@ impl EasyDapLink {
                     .width(200),
             ),
             grid_row!(
-                "Timeout (ms) for mount points",
+                "Timeout (s) for mount points",
                 number_input(
                     self.target_waiting_time,
                     TIMEOUT_MIN..=TIMEOUT_MAX,
                     Message::TimeoutChanged
                 )
-                .step(100)
+                .step(1)
                 .width(Length::Fill)
             ),
         )
@@ -254,7 +464,7 @@ impl Default for EasyDapLink {
             bootloader_path: PathBuf::default(),
             firmware_path: PathBuf::default(),
             user_file_path: PathBuf::default(),
-            target_waiting_time: 1000,
+            target_waiting_time: 10,
             target_name: String::default(),
             log_widget: LogWidget::default(),
         }
