55b9a4f9b5d04100ae4cfd9e06ba879146aa2027
Jonathan BAUDIN <jjbaudin@gmail.com>
2025-04-23 20:10:42 +0200

    feat: Add execution & script folder functions

--- src/dirs.rs
+++ src/dirs.rs
@@ -2,6 +2,8 @@ use std::{fs, path::PathBuf};
 
 use directories::ProjectDirs;
 
+static mut BASE_PATH: Option<PathBuf> = None;
+
 fn get_base_dir() -> Result<PathBuf, String> {
     match ProjectDirs::from("cc", "steami", "daplink-easyflash") {
         Some(b) => Ok(b.data_dir().to_path_buf()),
@@ -11,18 +13,47 @@ fn get_base_dir() -> Result<PathBuf, String> {
     }
 }
 
-pub fn get_script_dir() -> Result<PathBuf, String> {
-    let base = get_base_dir()?;
+pub fn set_exe_dir(pathbuf: PathBuf) {
+    unsafe {
+        BASE_PATH = Some(pathbuf);
+    }
+}
 
-    let script = base.join("scripts");
-
-    if !script.exists() && fs::create_dir_all(&script).is_err() {
-        match fs::create_dir_all(&script) {
-            Ok(_) => (),
-            Err(e) => {
-                return Err(format!("Failed to create script directory {e}"));
-            }
+pub fn get_exe_dir() -> Result<PathBuf, String> {
+    unsafe {
+        #[allow(static_mut_refs)]
+        match BASE_PATH.as_ref() {
+            Some(path) => Ok(path.clone()),
+            None => Err("No base path available. This error should not be happen !".into()),
         }
+    }
+}
+
+pub fn get_configs_dir() -> Result<PathBuf, String> {
+    let base = get_exe_dir()?;
+
+    let script = base.join("configs");
+
+    if !script.exists() {
+        return Err(format!(
+            "The configs folder does not exist (exe_dir: {})",
+            base.to_str().unwrap_or("not defined")
+        ));
+    }
+
+    Ok(script)
+}
+
+pub fn get_wireless_stack_dir() -> Result<PathBuf, String> {
+    let base = get_exe_dir()?;
+
+    let script = base.join("wireless_stack");
+
+    if !script.exists() {
+        return Err(format!(
+            "The wireless_stack folder does not exist (exe_dir: {})",
+            base.to_str().unwrap_or("not defined")
+        ));
     }
 
     Ok(script)
